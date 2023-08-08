36a3679c74ff451d81067e68c194a0f0b5f8e71c
legui <leguigol@hotmail.com>
2023-08-08 15:16:14 -0300

    ultimas modificaciones en wilde

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,32 +1,61 @@
 <script setup>
 
-import { ref } from 'vue';
+import { reactive } from 'vue';
 import {useUserStore} from '../stores/user';
 // import {useRouter} from 'vue-router';
 
+    const formState=reactive({
+        email:'leguigol@hotmail.com',
+        password: 'lancelot1014'
+    }) 
     const userStore=useUserStore();
-    const email=ref('');
-    const password=ref('');
+    // const email=ref('');
+    // const password=ref('');
     // const router=useRouter();
 
     const handleSubmit=async()=>{
-        if(!email.value || password.value.length<5){
-            return alert('llena los campos');
-        }
-        await userStore.loginUser(email.value,password.value);
+        // if(!email.value || password.value.length<5){
+        //     return alert('llena los campos');
+        // }
+        await userStore.loginUser(formState.email,formState.password);
         // router.push('/');
         console.log('procesando formulario...');
     }
 
+    const onFinish=async(values) => {
+        console.log("Success: ",values);
+        await userStore.loginUser(formState.email,formState.password);
+    }    
 
 </script>
 <template>
-    <div>
-        <h1>Login</h1>
-        <form @submit.prevent="handleSubmit">
-            <input type="email" placeholder="ingrese email" v-model.trim="email">
-            <input type="password" placeholder="ingrese password" v-model.trim="password">
-            <button type="submit" :disabled="userStore.loadingUser">Acceder</button>
-        </form>
-    </div>
+    <a-row>
+        <a-col :xs="{ span: 24}" :sm="{ span: 12, offset: 6 }">
+            <a-form name="basicLogin" autocomplete="off" layout="vertical" :model="formState" @finish="onFinish">
+                <a-form-item
+                    name="email"
+                    label="Ingresa tu correo"
+                    :rules="[{required: true, type: 'email', message:'Ingresa un email'}]"
+                >
+                    <a-input v-model:value="formState.email"></a-input>
+                </a-form-item>
+                <a-form-item
+                    name="password"
+                    label="Password"
+                    :rules="[{required: true, min: 6, message: 'debe tener 6 caracteres'},
+                    ]"
+                >
+                    <a-input-password v-model:value="formState.password"></a-input-password>
+                </a-form-item>   
+                <a-form-item>
+                    <a-button
+                        type="primary"
+                        html-type="submit"
+                        :disabled="userStore.loadingUser"
+                    >ACCEDER
+                    </a-button>
+                </a-form-item>
+            </a-form>
+        </a-col>
+    </a-row>
 </template>
